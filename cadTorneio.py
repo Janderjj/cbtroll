4371aae5be105cb529c9e082cd3ab9eaf4a579b6
jander gomes <151775496+Janderjj@users.noreply.github.com>
2024-05-17 08:34:10 -0300

    Update cadTorneio.py

--- cadTorneio.py
+++ cadTorneio.py
@@ -3,7 +3,7 @@ import json
 
 api_key = "RGAPI-5a8c30b5-d0bc-41c0-953f-788af715f5a7"
 url = 'https://americas.api.riotgames.com/lol/tournament-stub/v5/providers?api_key='+api_key
-dados = {"region": "BR1","url": "br1.api.riotgames.com"}
+dados = {"region": "BR1","url": "http://cbtroll.servegame.com/"}
 
 dados = json.dumps(dados)
 dados = str(dados)
